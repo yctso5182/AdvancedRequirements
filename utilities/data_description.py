38b9157cff6ad8c959e9f039bbe475f7e5ce9cc6
yctso5296 <yctso5296@gmail.com>
2022-04-19 17:28:51 +0800

    upload folders and files

---
+++ utilities/data_description.py
@@ -0,0 +1,19 @@
+### Modelnet Class labels in a list:
+class_names = ['airplane', 'bathtub', 'bed', 'bench', 'bookshelf', 'bottle',
+'bowl', 'car', 'chair', 'cone', 'cup', 'curtain', 'desk', 'door', 'dresser', 
+'flower pot', 'glass box', 'guiter', 'keyboard', 'lamp', 'laptop', 'mantel',
+'monitor', 'night stand', 'person', 'piano', 'plant', 'radio', 'range hood', 
+'sink', 'sofa', 'stairs', 'stool', 'table', 'tent', 'toilet', 'tv stand', 
+'vase', 'wardrobe', 'xbox'
+]
+
+### Modelnet data distribution classwise
+class_dist = [726, 156, 615, 193, 672, 435, 84, 297, 989, 187, 99, 
+158, 286, 129, 286, 169, 271, 255, 165, 144, 169, 348, 565, 286, 108, 331, 
+340, 124, 215, 148, 780, 144, 110, 492, 183, 444, 367, 575, 107, 123
+]
+
+# Class names vs class labels for S3DIS Dataset
+classes_s3dis = ['ceiling', 'floor', 'wall', 'beam', 'column',
+ 'window', 'door', 'table', 'chair', 'sofa', 'bookcase',
+  'board', 'clutter']
